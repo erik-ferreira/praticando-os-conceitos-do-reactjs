f8c5bc8f7b421434d4c93cf49c0334ba967519f4
erik-ferreira <erikdesouzagf@gmail.com>
2022-08-15 16:01:48 -0300

    complete layout

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { ClipboardText, Trash } from "phosphor-react";
 
 import { InputAddNewAssignment } from "./InputAddNewAssignment";
@@ -6,6 +7,7 @@ import styles from "./Main.module.scss";
 
 export function Main() {
   const haveNoTasks = false;
+  const [checked, setCheched] = useState(false);
 
   return (
     <section className={styles.containerSection}>
@@ -31,7 +33,7 @@ export function Main() {
 
         {haveNoTasks ? (
           <div className={styles.contentTasks}>
-            <ClipboardText color="#3D3D3D" />
+            <ClipboardText />
 
             <div className={styles.blockMessage}>
               <strong>Você ainda não tem tarefas cadastradas</strong>
@@ -41,16 +43,24 @@ export function Main() {
         ) : (
           <ul className={styles.listTasks}>
             <li>
-              <label>
-                <input type="checkbox" />
-                <span>
+              <div className={checked ? styles.completed : ""}>
+                <label className={styles.checkboxContainer}>
+                  <input
+                    type="checkbox"
+                    readOnly
+                    checked={checked}
+                    onClick={() => setCheched((prevState) => !prevState)}
+                  />
+                  <span className={styles.checkmark} />
+                </label>
+                <p>
                   Integer urna interdum massa libero auctor neque turpis turpis
                   semper. Duis vel sed fames integer.
-                </span>
-              </label>
+                </p>
+              </div>
 
-              <button type="button">
-                <Trash color="#808080" />
+              <button type="button" onClick={() => console.log("remover")}>
+                <Trash />
               </button>
             </li>
           </ul>
